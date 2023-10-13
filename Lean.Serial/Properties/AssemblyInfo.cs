65d4e0dcefa2015e8db001f6c5a86d82092ab08e
DavisCheng <leansoft365@outlook.com>
2023-10-13 14:19:29 +0800

    更新

--- Lean.Serial/Properties/AssemblyInfo.cs
+++ Lean.Serial/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Lean.Serial")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("序列号上传")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("TEAC")]
 [assembly: AssemblyProduct("Lean.Serial")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2023.10.0.0")]
+[assembly: AssemblyFileVersion("2023.10.0.0")]
